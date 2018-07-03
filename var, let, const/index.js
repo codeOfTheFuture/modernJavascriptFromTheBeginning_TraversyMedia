b5643ab98011cad9ca820857539bdf9ab68b02a3
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-03 03:27:55 -0400

    second commit

--- var, let, const/index.js
+++ var, let, const/index.js
@@ -1,17 +1,51 @@
 // Var, let, Const
 
-var name = 'John Doe';
-console.log(name);
-name = 'Steve Smith';
-console.log(name);
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
 
 // init var
 
-var greeting;
-console.log(greeting);
-greeting = 'Hello';
-console.log(greeting);
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
 
 // Letters, Numbers, _, $
 // Cannot start with a number
-var 1name = 'John';
+// var $name = 'John';
+// var _name = 'John';
+
+// Multi-word variables
+
+// var firstName = 'John'; // Camel Case
+// var first_name = 'Sara'; // Underscore
+// var FirstName = 'Tom'; // Pascal case
+// var firstname;
+
+//Let
+
+// let name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// Const
+
+// const name = 'John';
+// console.log(name);
+// Can not reassign
+// name = 'Sara';
+//Have to assign a value
+// const greeting;
+
+const person = {
+  name: 'John',
+  age: 30
+}
+
+person.name = 'Sara';
+person.age = 32;
+
+console.log(person);
