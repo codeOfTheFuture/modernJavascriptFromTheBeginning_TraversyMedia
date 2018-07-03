4de8dcd5737fb6dee39d063e3451ca9b0203d586
Jeff Oliver <jeffoliver3384@gmail.com>
2018-07-02 23:15:01 -0400

    first commit

---
+++ var, let, const/index.js
@@ -0,0 +1,17 @@
+// Var, let, Const
+
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+// init var
+
+var greeting;
+console.log(greeting);
+greeting = 'Hello';
+console.log(greeting);
+
+// Letters, Numbers, _, $
+// Cannot start with a number
+var 1name = 'John';
